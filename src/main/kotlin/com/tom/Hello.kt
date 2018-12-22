d38f0b5e4c6f0cd1b850ef841284d5871c4d4045
jessica <jessica811115@gmail.com>
2018-12-22 09:29:46 +0800

    20181222-1 myproject structure creation

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,13 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin"
+//    Human().hello();
+    val h = Human()
+    h.hello()
+}
+class  Human {
+    fun hello() {
+        println("hello kotlin")
+    }
+}
